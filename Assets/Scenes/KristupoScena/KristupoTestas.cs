37e31ed49e04a84e7b293ddc860ce5b80a1a19c0
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2021-01-30 18:40:10 +0200

    remake hero vision

--- Assets/Scenes/KristupoScena/KristupoTestas.cs
+++ Assets/Scenes/KristupoScena/KristupoTestas.cs
@@ -2,30 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KristupoTestas : MonoBehaviour, HeroVision
+public class KristupoTestas : MonoBehaviour
 {
-    public Transform player;
-    public Transform loot;
-    public Transform mob;
-    public HeroBehaviour hero;
-
-    public Transform GetPlayer()
-    {
-        return player;
-    }
-
-    public Transform GetClosestLoot()
-    {
-        return loot;
-    }
-
-    public Transform GetClosestMob()
-    {
-        return mob;
-    }
-
-    void Awake()
-    {
-        hero.vision = this;
-    }
 }
