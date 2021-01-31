0abee9aac84adffe10ed86f74da771b1f2db3d31
Simona <simonaglem@gmail.com>
2021-01-31 12:18:10 +0200

    Spawn units class update

--- Assets/Scripts/Units/SpawnUnits.cs
+++ Assets/Scripts/Units/SpawnUnits.cs
@@ -5,11 +5,6 @@ using UnityEngine.AI;
 
 public class SpawnUnits : MonoBehaviour
 {
-    /*enum SpawnType
-    {
-        Mob, Loot
-    };
-    public SpawnType spawnType;*/
     private List<GameObject> mobList = new List<GameObject>();
     public  GameObject Zombie;
     public  GameObject Shaman;
@@ -25,22 +20,19 @@ public class SpawnUnits : MonoBehaviour
         mobList.Add(Fallen);
         mobList.Add(Beast);
 
-        mobIndex = UnityEngine.Random.Range(0,4);
-
-        GenerateObject(mobList[mobIndex], count);
+        GenerateObject();
     }
 
-    void GenerateObject(GameObject go, int amount)
+    void GenerateObject()
     {
-        if (go == null) 
+        for (int i = 0; i < count; i++)
         {
-            return;
-        }
-
-        for (int i = 0; i < amount; i++)
-        {
-            //GameObject tmp = Instantiate(go);
             mobIndex = UnityEngine.Random.Range(0,4);
+
+            if (mobList[mobIndex] == null) 
+            {
+                continue;
+            }
 
             GameObject tmp = Instantiate(mobList[mobIndex]);
             
@@ -49,28 +41,12 @@ public class SpawnUnits : MonoBehaviour
             Vector3 randomPoint = GetRandomPoint();
             tmp.gameObject.transform.position = new Vector3(randomPoint.x, tmp.transform.position.y, randomPoint.z);
 
-            /*if (spawnType == SpawnType.Mob)
-            {
-                EventCoordinator.TriggerEvent(EventName.System.Environment.CreateMob(), GameMessage.Write().WithTargetTransform(tmp.transform));
-            }
-            else
-            {
-                EventCoordinator.TriggerEvent(EventName.System.Environment.CreateLoot(), GameMessage.Write().WithTargetTransform(tmp.transform));
-            }*/
             EventCoordinator.TriggerEvent(EventName.System.Environment.CreateMob(), GameMessage.Write().WithTargetTransform(tmp.transform));
         }
     }
 
     Vector3 GetRandomPoint()
     {
-        /*int xRandom = 0;
-        int zRandom = 0;
-
-        xRandom = (int)Random.Range(-5, 5);
-        zRandom = (int)Random.Range(-5, 5);
-        
-        return new Vector3(xRandom, 0.0f, zRandom) + transform.position;
-        */
         Vector3 newPos = RandomNavSphere(transform.position, 10, -1);
 
         return newPos + transform.position;
@@ -78,21 +54,9 @@ public class SpawnUnits : MonoBehaviour
 
     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) 
     {
-        /*Vector3 randDirection = Random.insideUnitCircle * dist;
-
-        randDirection += origin;
-
-        NavMeshHit navHit;
- 
-        if (NavMesh.SamplePosition(randDirection, out navHit, dist, layermask))
-        {
-            return navHit.position;
-        }
-        return Vector3.zero;*/
         NavMeshHit  navHit;
-        bool        canMove = false;
-
-        Vector3 randDirection = Random.insideUnitSphere * dist;
+        bool        canMove         = false;
+        Vector3     randDirection   = Random.insideUnitSphere * dist;
 
         randDirection += origin;
 
